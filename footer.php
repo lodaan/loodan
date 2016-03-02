bc7cedabf8a55b2f85859c853794935a4fab5fde
lodaan <loodaan83@gmail.com>
2016-03-02 16:47:44 +0200

    theme starter files

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package lodaan
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lodaan-bolg-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'lodaan-bolg-theme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'lodaan-bolg-theme' ), 'lodaan-bolg-theme', '<a href="http://lodaan.github.io/" rel="designer">lodaan</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
